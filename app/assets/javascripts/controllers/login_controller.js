3183a3cd2992cf90106dd87286f6734c097117a0
mo khan <mo@mokhan.ca>
2013-08-06 11:35:30 -0600

    use ember textfields for two way binding to controller

--- app/assets/javascripts/controllers/login_controller.js
+++ app/assets/javascripts/controllers/login_controller.js
@@ -1,5 +1,6 @@
 Timesheet.LoginController = Ember.Controller.extend({
   login: function(){
     console.log("LOGGING IN");
+    console.log(this.get('email') + ' ' + this.get('password'));
   }
 });
