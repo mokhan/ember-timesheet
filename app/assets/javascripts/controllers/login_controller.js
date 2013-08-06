e1c73dfe0024975c951a1830c10ecba4a917679f
mo khan <mo@mokhan.ca>
2013-08-06 14:20:33 -0600

    update login action on login controller

--- app/assets/javascripts/controllers/login_controller.js
+++ app/assets/javascripts/controllers/login_controller.js
@@ -1,7 +1,17 @@
 Timesheet.LoginController = Ember.Controller.extend({
   login: function(){
+    this.setProperties({ loginFailed: false, isProcessing: true});
     console.log("LOGGING IN");
     console.log(this.get('email') + ' ' + this.get('password'));
-    var users = Timesheet.User.find();
+    $.post("/login", { email: this.get('email'), password: this.get('password') })
+    .then(function(){
+      console.log('login SUCCESS');
+      this.set('isProcessing', false);
+      document.location = "/dashboard";
+    }.bind(this), function(){
+      console.log('login FAIL');
+      this.set('isProcessing', false);
+      this.set('loginFailed', true);
+    }.bind(this));
   }
 });
