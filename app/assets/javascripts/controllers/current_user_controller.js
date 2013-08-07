12ab0fb78cd79ce16fc250c4aa99922d5bac9742
mo khan <mo@mokhan.ca>
2013-08-06 23:19:26 -0600

    remove unused user method

--- app/assets/javascripts/controllers/current_user_controller.js
+++ app/assets/javascripts/controllers/current_user_controller.js
@@ -2,9 +2,5 @@ Timesheet.CurrentUserController = Ember.ObjectController.extend({
   isSignedIn: function() {
     var user = this.get('model');
     return user && user.get('isLoaded');
-  }.property('model.isLoaded'),
-
-  user: function() {
-    return this.get('model');
   }.property('model.isLoaded')
 });
