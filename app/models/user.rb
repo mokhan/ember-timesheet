01f8ca3df61f32e87f8af32765a070075bcc0432
mo khan <mo@mokhan.ca>
2013-08-07 21:14:39 -0600

    create an auth token when a new user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  after_create :create_auth_token
+
+  private
+
+  def create_auth_token
+    self.update_attribute(:authentication_token, SecureRandom.hex)
+  end
 end
